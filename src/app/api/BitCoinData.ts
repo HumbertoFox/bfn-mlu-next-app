e12d385adad00c54e0f1f796b12f29d93b3c6e07
Humberto Ribeiro <humbertoemaus@gmail.com>
2024-11-29 04:32:54 -0300

    Base Project

---
+++ src/app/api/BitCoinData.ts
@@ -0,0 +1,31 @@
+// Tipagem da função getBitcoinData
+export interface BitcoinData {
+    timestamp: number;
+    price: number;
+}
+
+// Tipo dos dados retornados pela API
+interface ApiResponse {
+    prices: [number, number][]; // Array de tuplas [timestamp, price]
+}
+
+export const getBitcoinData = async (): Promise<BitcoinData[]> => {
+    try {
+        const response = await fetch('https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=1');
+        const data: ApiResponse = await response.json(); // Tipando a resposta da API
+
+        // Verificar se 'data' contém a chave 'prices' e se ela é um array
+        if (data && Array.isArray(data.prices)) {
+            // Mapeia o array de preços (preço, timestamp) para o formato BitcoinData
+            return data.prices.map(([timestamp, price]): BitcoinData => ({
+                timestamp,
+                price,
+            }));
+        } else {
+            throw new Error('A resposta da API não contém dados de preços válidos');
+        }
+    } catch (error) {
+        console.error('Erro ao buscar os dados do Bitcoin:', error);
+        return []; // Retorna um array vazio em caso de erro
+    }
+};
---
+++ src/app/page.tsx
@@ -0,0 +1,98 @@
+'use client'
+
+import { useEffect, useState } from 'react';
+import { Line } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+import { BitcoinData, getBitcoinData } from '@/app/api/BitCoinData';
+import Head from 'next/head';
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+export default function Home() {
+  const [bitcoinData, setBitcoinData] = useState<BitcoinData[]>([]);
+  const [chartData, setChartData] = useState({
+    labels: [] as string[],
+    datasets: [
+      {
+        label: 'Bitcoin (USD)',
+        data: [] as number[],
+        borderColor: '#F7931A',
+        backgroundColor: 'rgba(247, 147, 26, 0.2)',
+        borderWidth: 1,
+      },
+    ],
+  });
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Função para atualizar o estado do gráfico
+  const updateChartData = (labels: string[], data: number[]) => {
+    setChartData((prevChartData) => ({
+      ...prevChartData,
+      labels,
+      datasets: [
+        {
+          ...prevChartData.datasets[0],
+          data,
+        },
+      ],
+    }));
+  };
+
+  // Efeito para buscar os dados do Bitcoin
+  useEffect(() => {
+    const fetchBitcoinData = async () => {
+      setIsLoading(true);
+      const data = await getBitcoinData();
+      setBitcoinData(data);
+      setIsLoading(false);
+    };
+
+    fetchBitcoinData();
+    const interval = setInterval(fetchBitcoinData, 60000); // Atualiza a cada 1 minuto
+
+    return () => clearInterval(interval); // Limpa o intervalo quando o componente desmonta
+  }, []);
+
+  // Efeito para atualizar o gráfico quando os dados do Bitcoin são recebidos
+  useEffect(() => {
+    if (bitcoinData.length > 0) {
+      const labels = bitcoinData.map((data) => new Date(data.timestamp).toLocaleTimeString());
+      const data = bitcoinData.map((data) => data.price);
+
+      updateChartData(labels, data);
+    }
+  }, [bitcoinData]);
+
+  return (
+    <>
+      <Head>
+        <title>Bitcoin Value in the Last 24 Hours - ${bitcoinData[bitcoinData.length - 1]?.price || 'Loading'}</title>
+        <meta
+          name='description'
+          content={`Current Bitcoin price is ${bitcoinData[bitcoinData.length - 1]?.price || 'loading'} USD`}
+        />
+      </Head>
+
+      <div className='w-full min-h-screen flex flex-col justify-between p-5'>
+        <main className='flex flex-col gap-8 row-start-2 items-center'>
+          <h1 className='text-3xl font-bold'>Bitcoin Value in the Last 24 Hours</h1>
+
+          <div className='w-full h-[50vh] max-h-[500px] flex justify-center p-2'>
+            {isLoading ? (
+              <p>Loading data...</p>
+            ) : (
+              <Line
+                data={chartData}
+                options={{ responsive: true }}
+              />
+            )}
+          </div>
+        </main>
+
+        <footer className='row-start-3 flex gap-6 flex-wrap items-center justify-center'>
+          <p>&copy; 2024 Bitcoin Data</p>
+        </footer>
+      </div>
+    </>
+  );
+};
