26bf6f8b1b5154e3691bf9d3cb2cb97dad4d297d
Humberto Ribeiro <humbertoemaus@gmail.com>
2025-01-29 04:32:47 -0300

    update add pages

---
+++ src/app/logins/page.tsx
@@ -0,0 +1,38 @@
+import SignInForm from '@/components/ui/signinform';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Login() {
+    return (
+        <div className='w-full h-[90svh] flex items-center justify-center'>
+            <div className='w-96 flex flex-col items-center justify-center gap-5 rounded-lg shadow shadow-blue-400 p-3'>
+                <Link href={'/'}>
+                    <Image
+                        className='pt-6'
+                        src={'/images/bitcoin-user.png'}
+                        width={75}
+                        height={75}
+                        alt='Icon Usuário Biticoin'
+                        priority
+                    />
+                </Link>
+                <SignInForm />
+                <div className='w-full flex justify-around'>
+                    <Link
+                        className='text-xs text-gray-400 hover:text-black duration-500'
+                        href={'/'}
+                    >
+                        Esqueci a Senha!
+                    </Link>
+
+                    <Link
+                        className='text-xs text-gray-400 hover:text-black duration-500'
+                        href={'/signup'}
+                    >
+                        Não tenho Cadastro!
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
